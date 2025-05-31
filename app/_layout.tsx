adff2d4c7ab590b67d2886ca04c07380aa0ded81
jennifer <jenniferkasstudio@gmail.com>
2025-05-31 23:51:15 +0800

    feat(samples): add font ttf

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
 
 export default function RootLayout() {
+  let [fontsLoaded] = useFonts({
+    eryaxindahei_T: require("../assets/fonts/eryaxindahei_T.ttf"),
+    montserrat_variableFont_wght: require("../assets/fonts/montserrat_variableFont_wght.ttf"),
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(samples)" />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
